a105b5b31a1c207f906efe0d5b10ccd2d7ab262b
GH-Ivan <71291945+GH-Ivan@users.noreply.github.com>
2020-10-30 20:25:29 -0400

    Add files via upload

---
+++ Job_Creator.py
@@ -0,0 +1,38 @@
+import socket
+
+#Default TCP IP and Port
+TCP_IP = '127.2.3.4'  
+TCP_PORT = 52158        
+run = True
+jobs = 3
+data = b''
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+
+HASC = False
+
+while(run):
+    if not HASC:
+        print("Looking for seekers...\n")
+        s.listen()
+        conn, addr = s.accept()
+        print("Found seeker!\nAwaiting confirmation...\n")
+        data =b''
+        HASC = True
+
+    conn.send(b'Here is a job')
+    while True:
+            data = conn.recv(1024)
+            if data == b'No':
+                print("Job refused :(")
+                break
+            else:
+                conn.recv(1024)
+                print("Job Complete")
+                jobs-=1
+                break
+    if jobs <=0:
+        conn.send(b'T')
+        run = False
+            
+            
---
+++ Job_Seeker.py
@@ -0,0 +1,42 @@
+import socket
+import time
+import threading
+
+#Default TCP IP and Port
+TCP_IP = '127.2.3.4'  
+TCP_PORT = 52158
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+data = b''
+
+run = True
+
+Connected = False
+
+while run:
+
+    try:
+        data = s.recv(1024)
+    except:
+        s.connect((TCP_IP, TCP_PORT))
+        data = s.recv(1024)
+
+
+    print("Looking for job...")
+
+    #If the job creator has no more jobs
+    if data == b'T':
+        run = False
+        break
+    ui = input("Found Job, Accept? y/n\n")
+
+    if ui in ["y","Y"]:
+        s.send(b'Yes')
+        time.sleep(1)
+        s.send(b'done')
+
+        print("Job finished")
+    else:
+        s.send(b'No')
+        time.sleep(1)
+        print("Job refused")
+    
